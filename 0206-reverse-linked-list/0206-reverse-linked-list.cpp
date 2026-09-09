a06f708fecf1edfe910ce091cf0f5fbc73ac97cc
Manav <engmanav01@gmail.com>
2026-09-10 02:52:16 +0530

    Time: 0 ms (100%), Space: 13.4 MB (72.25%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* prev = NULL;
+        ListNode* cur = head;
+
+        while (cur != NULL) {
+
+            ListNode* next = cur->next;
+
+            cur->next = prev;
+
+            prev = cur;
+
+            cur = next;
+        }
+
+        return prev;
+    }
+};
